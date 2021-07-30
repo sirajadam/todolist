c8865feb64ae612828447053ed3841b073cd2007
adam <adamsirajdeveloper@gmail.com>
2021-07-30 02:30:04 +0200

    added localStorage and refactored script.js

--- script.js
+++ script.js
@@ -3,53 +3,102 @@
 const submitBtn = document.querySelector(".submit-btn");
 const taskContainer = document.querySelector(".task-container");
 
-// ============ Adding Task ============
+// initialize localStorage
+
+const todos = JSON.parse(localStorage.getItem("tasks"));
+
+if (todos) {
+  todos.forEach((todo) => {
+    addTodo(todo);
+  });
+}
+
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
 
-  // Look to see if bad practice
+  addTodo();
+});
+
+function addTodo(todo) {
+  // ============ Adding Task ============
   let taskInput = document.querySelector(".form-input").value;
+  let completed = false;
+
+  // ============ Clearing input field ============
+  document.querySelector(".form-input").value = "";
+
+  if (todo) {
+    taskInput = todo.text;
+    completed = todo.completed;
+  }
+
+  // ============ Creating todo element ============
+  createTodoEl(taskInput, completed);
+
+  // ============ Completing Task ============
+  completeTask();
+
+  // ============ Deleting Task ============
+  deleteTask();
+}
+
+function createTodoEl(taskName, completedStatus) {
   const alert = document.querySelector(".alert");
 
   const taskHtml = `<div class="row task">
-  <span class="task-name">${taskInput}</span>
+  <span class="task-name ${
+    completedStatus ? "completed" : ""
+  }">${taskName}</span>
   <button type="submit" class="btn delete-btn">
   <i class="uil uil-trash-alt"></i>
   </button>
   </div>`;
 
-  if (taskInput.length == 0) {
+  if (taskName.length == 0) {
     alert.textContent = "please enter a to-do";
     // showing alert text
     alert.classList.add("alert-danger");
   } else {
-    taskContainer.insertAdjacentHTML("afterbegin", taskHtml);
-    // removing alert text
+    taskContainer.insertAdjacentHTML("beforeend", taskHtml);
+    updateLocalStorage();
     alert.classList.remove("alert-danger");
   }
+}
 
-  // ============ Clearing input field ============
-  document.querySelector(".form-input").value = "";
+function deleteTask() {
+  const delBtn = document.querySelectorAll(".delete-btn");
 
-  // ============ Completing Task ============
+  delBtn.forEach((task) =>
+    task.addEventListener("click", function (e) {
+      e.preventDefault();
+      this.parentNode.remove();
+      updateLocalStorage();
+    })
+  );
+}
+
+function completeTask() {
   const task = document.querySelectorAll(".task-name");
   task.forEach(
     (task) =>
       (task.onclick = function () {
         this.classList.toggle("completed");
+        updateLocalStorage();
       })
   );
-  // Check for bug: some elements don't click or click 3-4 times
-  // Fixed the bug by using onclick. For some reason it worked. Find out why.
+}
 
-  //   Try using event propogation if fit
+function updateLocalStorage() {
+  const todosEl = document.querySelectorAll(".task-name");
 
-  // ============ Deleting Task ============
-  const delBtn = document.querySelectorAll(".delete-btn");
-  delBtn.forEach((task) =>
-    task.addEventListener("click", function (e) {
-      e.preventDefault();
-      this.parentNode.remove();
-    })
-  );
-});
+  const taskArray = [];
+
+  todosEl.forEach((task) => {
+    taskArray.push({
+      text: task.innerHTML,
+      completed: task.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(taskArray));
+}
